73a2419a09678be06c8eae30c629a7e41ab17d61
Bill XIE <persmule@hardenedlinux.org>
2019-10-28 00:16:05 +0800

    mb/lenovo/t540p/mainboard: Make dock init in ramstage fully mainboard-specific
    Discussed in CB:36093, in the past many lenovo boards need to declare
    an empty h8_mainboard_init_dock() to satisfy h8.c.
    
    Now the confusing H8_DOCK_EARLY_INIT might be retired, and if a
    mainboard needs dock init (done with h8_mainboard_init_dock() in the
    past) in ramstage, (discussed in CB:4294 where H8_DOCK_EARLY_INIT is
    introduced) it can just do it in its own chip_ops.enable_dev function.
    
    Tested on X200. Testing on other affected targets may be necessary.
    
    See also commit 72f13e53 with Change-Id:
    I5737406d1f6cb6e91b2e2fa349a206a3dba988d1 ("ec/lenovo/h8: Make dock init
    in ramstage fully mainboard-specific").
    
    Change-Id: Ib56b9d8c634de6692cfee6f698b1f230dc0f580b
    Signed-off-by: Bill XIE <persmule@hardenedlinux.org>
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/mainboard/lenovo/t540p/mainboard.c
+++ src/mainboard/lenovo/t540p/mainboard.c
@@ -8,10 +8,6 @@ static void mainboard_enable(struct device *dev)
 	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS, GMA_INT15_PANEL_FIT_DEFAULT, GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
 }
 
-void h8_mainboard_init_dock(void)
-{
-}
-
 struct chip_operations mainboard_ops = {
 	.enable_dev = mainboard_enable,
 };
